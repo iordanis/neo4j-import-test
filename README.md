c54c66e4e42fba93ff875522d275fab208365a4c
Iordanis Giannakakis <i.giannakakis@ymail.com>
2012-04-28 13:46:51 +0100

    Added neo4j server and configuration

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 neo4j-import-test
 =================
+Achieves for 1000 records something like:
+
+* embedded : 12 rps 
+* REST: 6.25 rps
---
+++ src/main/java/com/nextmilestone/importer/ImporterConfiguration.java
@@ -0,0 +1,16 @@
+package com.nextmilestone.importer;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.data.neo4j.rest.SpringRestGraphDatabase;
+
+@Configuration
+public class ImporterConfiguration {
+
+	@Bean
+	@Scope(value="singleton")
+	public SpringRestGraphDatabase graphDatabaseService() {
+		return new SpringRestGraphDatabase("http://localhost:7474/db/data/");
+	}
+}
